23bfb6cc6db180b249874fda82b09ac999bd5180
Uselight <uselight@mail.ru>
2015-11-03 20:38:15 +0300

    RegExTest
    A tool to test regular expressions

---
+++ RegExTest/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace RegExTest {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.txtExpression = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.memSample = new DevExpress.XtraEditors.MemoEdit();
+            this.memMatches = new DevExpress.XtraEditors.MemoEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.btnFindMatches = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.txtExpression.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memSample.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memMatches.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtExpression
+            // 
+            this.txtExpression.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtExpression.EditValue = "\\bS\\S*";
+            this.txtExpression.Location = new System.Drawing.Point(30, 36);
+            this.txtExpression.Name = "txtExpression";
+            this.txtExpression.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.txtExpression.Properties.Appearance.Options.UseFont = true;
+            this.txtExpression.Size = new System.Drawing.Size(657, 26);
+            this.txtExpression.TabIndex = 0;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelControl1.Location = new System.Drawing.Point(30, 12);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(127, 18);
+            this.labelControl1.TabIndex = 1;
+            this.labelControl1.Text = "Regular Expression:";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Appearance.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelControl2.Location = new System.Drawing.Point(30, 89);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(88, 18);
+            this.labelControl2.TabIndex = 2;
+            this.labelControl2.Text = "Sample Text:";
+            // 
+            // memSample
+            // 
+            this.memSample.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.memSample.EditValue = "A Thousand Splendid Suns";
+            this.memSample.Location = new System.Drawing.Point(30, 122);
+            this.memSample.Name = "memSample";
+            this.memSample.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.memSample.Properties.Appearance.Options.UseFont = true;
+            this.memSample.Size = new System.Drawing.Size(657, 96);
+            this.memSample.TabIndex = 3;
+            // 
+            // memMatches
+            // 
+            this.memMatches.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.memMatches.Location = new System.Drawing.Point(30, 316);
+            this.memMatches.Name = "memMatches";
+            this.memMatches.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.memMatches.Properties.Appearance.Options.UseFont = true;
+            this.memMatches.Size = new System.Drawing.Size(657, 220);
+            this.memMatches.TabIndex = 5;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Font = new System.Drawing.Font("Tahoma", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelControl3.Location = new System.Drawing.Point(30, 283);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(60, 18);
+            this.labelControl3.TabIndex = 4;
+            this.labelControl3.Text = "Matches:";
+            // 
+            // btnFindMatches
+            // 
+            this.btnFindMatches.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnFindMatches.Location = new System.Drawing.Point(248, 237);
+            this.btnFindMatches.Name = "btnFindMatches";
+            this.btnFindMatches.Size = new System.Drawing.Size(196, 35);
+            this.btnFindMatches.TabIndex = 6;
+            this.btnFindMatches.Text = "Find Matches";
+            this.btnFindMatches.UseVisualStyleBackColor = true;
+            this.btnFindMatches.Click += new System.EventHandler(this.btnFindMatches_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 548);
+            this.Controls.Add(this.btnFindMatches);
+            this.Controls.Add(this.memMatches);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.memSample);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.txtExpression);
+            this.MinimumSize = new System.Drawing.Size(512, 380);
+            this.Name = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.txtExpression.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memSample.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.memMatches.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.TextEdit txtExpression;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.MemoEdit memSample;
+        private DevExpress.XtraEditors.MemoEdit memMatches;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private System.Windows.Forms.Button btnFindMatches;
+    }
+}
+
---
+++ RegExTest/Form1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace RegExTest {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnFindMatches_Click(object sender, EventArgs e) {
+            memMatches.Text = String.Empty;
+            string expr = txtExpression.Text;
+            string str = memSample.Text;
+
+            MatchCollection mc = Regex.Matches(str, expr);
+            memMatches.Lines = mc.Cast<Match>().Select(match => match.Value).ToArray(); ;
+        }
+
+    }
+}
